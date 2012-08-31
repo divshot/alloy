e2ed63b9253548d3939d836210445dbf5ece8d6d
Michael Bleigh <mbleigh@mbleigh.com>
2012-08-31 10:56:15 -0700

    Adds bookmarklet section.

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ module Alloy
       use Alloy::Throttle, max: 100, cache: $redis
     end
 
+    helpers do
+      def bookmarklet_code(type)
+        source = <<-JS
+        javascript:(function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.3.2",function($,L){$("body").append("<div id='divshot-alloy-message' style='-webkit-border-radius: 4px; border-radius: 4px; position:fixed; font-size: 18px; bottom: 15px; left: 50%; margin-left: -300px; background: rgba(0,0,0,0.75); padding: 4px 10px; width: 600px; text-align: center; font-family: sans-serif; color: white;'>Click any textarea element to compile #{type} into CSS</div>");var action=function(){$("*").unbind("click",action);$("#divshot-alloy-message").remove();if($(this).is("textarea")){var a=$(this);var b=$(this).val();a.val("Compiling, please wait...");$.ajax({type:"POST",dataType:"text",url:"http://alloy.divshot.com/compile",data:{source:b,type:"#{type.downcase}",},success:function(c){a.val(c)},error:function(){alert("Something went wrong.");a.val(b)}})}else{alert("You didn't click a textarea.")}return false};$("*").bind("click",action);});
+        JS
+        Rack::Utils.escape_html(source.strip)
+      end
+    end
+
     use Rack::Cors do
       allow do
         origins '*'
